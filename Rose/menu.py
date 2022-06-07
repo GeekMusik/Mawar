853ef82315dfdc4ddb38754a643aaf8ba8d34af9
Geek Musik <106485862+GeekSuppurt@users.noreply.github.com>
2022-06-07 19:20:43 +0700

    Add files via upload

---
+++ Rose/menu.py
@@ -0,0 +1,88 @@
+from Rose import bot as app
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
+from Rose.utils.lang import *
+
+
+fbuttons = InlineKeyboardMarkup(
+        [
+        [
+            InlineKeyboardButton(
+                text="👥Support Group", url="https://t.me/GeekMusik"
+            ),
+            InlineKeyboardButton(
+                text="👤News Channel", url="https://t.me/MH_Projects"
+            )
+        ], 
+        [
+            InlineKeyboardButton(
+                text="⚒ Source Code", url="https://github.com/GeekMusik/Mawar"
+            ),
+            InlineKeyboardButton(
+                text="📓 Documentation", url="https://geekmusic.mhpay.repl.co/mawar"
+            )
+        ], 
+        [
+            InlineKeyboardButton(
+                text="🖥 How To Deploy Me", url="https://geekmusic.mhpay.repl.co/mawar"
+            )
+        ], 
+        [
+            InlineKeyboardButton("« Back", callback_data='startcq')
+        ]
+        ]
+)
+
+keyboard =InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                text="English🇬🇧", callback_data="languages_en"
+            ),
+            InlineKeyboardButton(
+                text="Indonesia 🇮🇩", callback_data="languages_id"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="हिन्दी🇮🇳", callback_data="languages_hi"
+            ),
+            InlineKeyboardButton(
+                text="Italiano🇮🇹", callback_data="languages_it"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="🌎 Help us with translation",
+                url=f"https://crwd.in/mawar",
+            )
+        ],
+        [
+            InlineKeyboardButton("« Back", callback_data='startcq')
+        ]
+    ]
+)
+
+@app.on_callback_query(filters.regex("_langs"))
+@languageCB
+async def commands_callbacc(client, CallbackQuery, _):
+    user = CallbackQuery.message.from_user.mention
+    await app.send_message(
+        CallbackQuery.message.chat.id,
+        text= _["setting_1"].format(user),
+        reply_markup=keyboard,
+        disable_web_page_preview=True,
+    )
+    await CallbackQuery.message.delete()
+    
+@app.on_callback_query(filters.regex("_about"))
+@languageCB
+async def commands_callbacc(client, CallbackQuery, _):
+    await app.send_message(
+        CallbackQuery.message.chat.id,
+        text=_["menu"],
+        reply_markup=fbuttons,
+        disable_web_page_preview=True,
+    )
+    await CallbackQuery.message.delete()
+
